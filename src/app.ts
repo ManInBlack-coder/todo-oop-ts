9aea2213ff4539fddae370e7aadfb38ae152b306
ManInBlack-coder <aleksq066@gmail.com>
2024-01-23 14:34:12 +0200

    Created project basic files

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request,Response,NextFunction } from 'express'
+import todoRoutes from './routes/todos'
+
+
+const app = express()
+
+app.use('/todos',todoRoutes)
+
+app.use((error: Error, req: Request,res: Response,next: NextFunction) => {
+    res.json({nessage: error.message})
+})
+
+app.listen(3011,() => {
+    console.log('program is started at port 3011')
+})
