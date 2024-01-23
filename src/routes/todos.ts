e711b16c4248e357c958ae52b3e68d09a9e2bda2
ManInBlack-coder <aleksq066@gmail.com>
2024-01-23 15:09:56 +0200

    Create new Todo from user data

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { createTodo } from "../controllers/todos";
 
 const router = Router()
 
 
+router.post('/',createTodo)
 router.post('/')
 router.get('/')
 router.patch('/:id')
